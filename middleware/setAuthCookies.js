0b647c6fb08b441b9fd30e89793176fd3ba6db6a
Piyush Goyal <piyush2022ug@gmail.com>
2025-03-29 10:23:56 +0530

    fix: setAuthCookies config

--- middleware/setAuthCookies.js
+++ middleware/setAuthCookies.js
@@ -2,14 +2,14 @@ function setAuthCookies(res, accessToken, refreshToken) {
   res.cookie("access_token", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: 15 * 60 * 1000,
   });
 
   res.cookie("refresh_token", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     path: "/api/user/refresh-token",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
