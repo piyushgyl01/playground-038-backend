bc990c077620143e22c290b6b328255e1336ac08
Piyush Goyal <piyush2022ug@gmail.com>
2025-03-30 10:51:22 +0530

    chore: file-cleanup

--- middleware/setAuthCookies.js
+++ middleware/setAuthCookies.js
@@ -1,22 +1,21 @@
 
 function setAuthCookies(res, accessToken, refreshToken) {
-  // For production deployment with cross-domain cookies
   const isProduction = process.env.NODE_ENV === "production";
   
   res.cookie("access_token", accessToken, {
     httpOnly: true,
-    secure: isProduction, // Must be true in production
-    sameSite: isProduction ? "none" : "lax", // "none" allows cross-domain cookies
-    path: "/", // Accessible at all paths
-    maxAge: 15 * 60 * 1000, // 15 minutes
+    secure: isProduction, 
+    sameSite: isProduction ? "none" : "lax", 
+    path: "/", 
+    maxAge: 15 * 60 * 1000,
   });
 
   res.cookie("refresh_token", refreshToken, {
     httpOnly: true,
-    secure: isProduction, // Must be true in production
-    sameSite: isProduction ? "none" : "lax", // "none" allows cross-domain cookies
-    path: "/api/user/refresh-token", // Match your refresh endpoint
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    path: "/api/user/refresh-token",
+    maxAge: 7 * 24 * 60 * 60 * 1000, 
   });
 }
 
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,31 +1,7 @@
-// const jwt = require("jsonwebtoken");
-
-// function verifyJWT(req, res, next) {
-//   const authHeader = req.headers.authorization || req.headers.Authorization;
-
-//   if (!authHeader?.startsWith("Token ")) {
-//     return res.status(401).json({ message: "Unauthorized" });
-//   }
-
-//   const token = authHeader.split(" ")[1];
-
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(403).json({ message: "Forbidden" });
-//     }
-//     req.userId = decoded.user.id;
-//     req.userEmail = decoded.user.email;
-//     req.userHashedPwd = decoded.user.password;
-//     next();
-//   });
-// }
-
-// module.exports = verifyJWT;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 function verifyJWT(req, res, next) {
   const accessToken = req.cookies.access_token;
